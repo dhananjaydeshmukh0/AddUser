0899f9cbea0d68e38fd9870aedc6a6d5be116294
Dhananjay <deshmukhdhananjay06@gmail.com>
2022-08-10 14:42:21 +0530

    Hey

---
+++ src/Users/AddUser.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const AddUser = (props)=>{
+    const addUserHandler= (event)=>{
+        event.preventDefault();
+    }
+    return (
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="" id="username" />
+            <label htmlFor='age'>Age(Year)</label>
+            <input type="number" name="age" id="age" />
+            <button type="submit">Add User</button>
+        </form>
+    )
+}
+export default AddUser
